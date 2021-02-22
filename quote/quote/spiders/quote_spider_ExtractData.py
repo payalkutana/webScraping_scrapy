95d2135d81142c3df18f36360e8497a72098494b
payal kutana <46380440+payalkutana@users.noreply.github.com>
2021-02-22 16:52:05 +0530

    Add files via upload

---
+++ quote/quote/spiders/quote_spider_ExtractData.py
@@ -0,0 +1,22 @@
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+    name='quotes2'
+    start_urls=[
+        'http://quotes.toscrape.com/'
+    ]
+
+    def parse(self,response):
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
+
+            yield {
+                'title':title,
+                'author':author,
+                'tag':tag
+            }
+            
